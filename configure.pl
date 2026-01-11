85c9397e6e50910d20f49ef0ccafbdab9f3786df
Amlal El Mahrouss <amlal@nekernel.org>
2026-01-11 18:59:37 +0100

    feat: configure.pl script to build NeKernel MinGW.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

---
+++ configure.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Cwd;
+
+if ($ENV{USER} ne "root") {
+    die "You must be root to run this script.\n";
+}
+
+system("sh", "git", "submodule", "update", "--init", "--recursive");
+
+chdir("./toolchains/mingw-w64-nekernel/") or die "Cannot change directory: $!";
+
+system("sh", "configure", "--host=x86_64-w64-mingw32", "--prefix=mingw64-w64-nekernel") or die "Configuration failed: $!";
+system("make", "install") or die "Build failed: $!";
+
+print("Configuration and build complete.\nUse 'make install' to install the toolchain.\n");
