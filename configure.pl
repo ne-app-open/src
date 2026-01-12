3f0996e573a849a29f1043df5daeb32c1ed128cf
Amlal El Mahrouss <amlal@nekernel.org>
2026-01-12 13:04:41 +0100

    chore: configure.pl: don't run `install` after configuring mingw repo.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- configure.pl
+++ configure.pl
@@ -12,6 +12,5 @@ system("sh", "git", "submodule", "update", "--init", "--recursive");
 chdir("./toolchains/mingw-w64-nekernel/") or die "Cannot change directory: $!";
 
 system("configure", "--host=x86_64-w64-mingw32", "--prefix=mingw64-w64-nekernel") or die "Configuration failed: $!";
-system("make", "install") or die "Build failed: $!";
 
 print("Configuration and build complete.\nUse 'make install' to install the toolchain.\n");
