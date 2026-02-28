9b84b72a325e76131fe7b052733ce94611feb9f6
Amlal El Mahrouss <amlal@nekernel.org>
2026-02-28 14:01:18 +0100

    chore: updater.sh improvements from OCL.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- tools/updater.py
+++ tools/updater.py
@@ -1,18 +1,22 @@
-#! /usr/bin/python3
+# Copyright 2026, Amlal El Mahrouss and Ne.org contributors.
+# Open C++ Libraries is licensed under BSL-1.0
 
 from os import system
 
-# Copyright 2026, Amlal El Mahrouss and Ne.org contributors.
-# The Ne Syste, is licensed under the BSL-1.0 or Apache 2.0.
+class UpdateFunctor:
+    def __init__(self):
+        system("cd libs && cd kernel && git pull && cd ..")
+        system("cd libs && cd nectar && git pull && cd ..")
+        system("cd libs && cd build && git pull && cd ..")
+        system("cd libs && cd boot && git pull && cd ..")
+        system("cd papers && git pull")
+        system("git add src/kernel src/nectar papers src/build src/boot")
+        system("git commit -s -S")
 
-if __name__ == '__main__':
-    print("precommit: running format.sh...")
-    system("cd libs && cd kernel && git pull && cd ..")
-    system("cd libs && cd nectar && git pull && cd ..")
-    system("cd libs && cd build && git pull && cd ..")
-    system("cd libs && cd boot && git pull && cd ..")
-    system("cd papers && git pull")
-    system("git add src/kernel src/nectar papers src/build src/boot")
-    system("git commit -s -S")
+def start():
+    functor = UpdateFunctor()
+    print("INFO: Updater: Functor called")
+
+
 
 
