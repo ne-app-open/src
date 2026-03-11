b5ca51474e67d9e238b62499986923fc08e5e5c5
Amlal El Mahrouss <amlal@nekernel.org>
2026-03-11 15:49:13 +0100

    [CHORE] Add SPDX for System and Drivers headers.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- include/System/X11+Fwd.h
+++ include/System/X11+Fwd.h
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: Apache-2.0
 // Copyright 2026, Amlal El Mahrouss (amlal@nekernel.org)
 // Licensed under the Apache License, Version 2.0 (see LICENSE file)
 // Official repository: https://github.com/ne-foss-org/src
