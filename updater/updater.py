1eb441d2f2755034997a7102d41305a27ee9c7aa
Amlal El Mahrouss <amlal@nekernel.org>
2026-03-14 08:06:30 +0100

    [FEAT] src/kernel features for BootZ, X11 and GPUBasic driver in WiP.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- updater/updater.py
+++ updater/updater.py
@@ -6,10 +6,10 @@ from os import system
 
 class UpdateFunctor:
     def __init__(self):
-        system("cd libs && cd kernel && git pull && cd ..")
-        system("cd libs && cd nectar && git pull && cd ..")
-        system("cd libs && cd build && git pull && cd ..")
-        system("cd libs && cd boot && git pull && cd ..")
+        system("cd src && cd kernel && git pull && cd ..")
+        system("cd src && cd nectar && git pull && cd ..")
+        system("cd src && cd build && git pull && cd ..")
+        system("cd src && cd boot && git pull && cd ..")
         system("cd papers && git pull")
         system("git add src/kernel src/nectar papers src/build src/boot")
         system("git commit -s -S")
