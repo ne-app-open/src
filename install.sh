17aad86e9633506f1ebff74f5b34f1255e4ed404
Amlal El Mahrouss <amlal@nekernel.org>
2026-03-27 12:15:42 +0100

    [CHORE] Update install.sh script.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- install.sh
+++ install.sh
@@ -2,13 +2,16 @@
 
 ## To be used for: curl -fsSL https://install.nekernel.org | sh
 
-echo "== WELCOME TO NESYSTEM =="
-echo "INSTALLING NESYSTEM..."
+echo "==> WELCOME TO NESYSTEM."
+echo "==> INSTALLING NESYSTEM..."
+
+sudo apt update
+sudo apt install nasm mingw-w64 x86_64-w64-mingw32-g++ x86_64-w64-mingw32-gcc build-essential cmake libboost-dev
 
 ARCH=x64
 PROFILE=debug
 
-git clone --recurse-submodules -j8 git@github.com:ne-foss-org/ne-system.git
+git clone --recurse-submodules -j8 https://github.com/ne-foss-org/ne-system.git
 cd ne-system
 export UPDATER_NO_COMMIT=1
 ./updater.sh
