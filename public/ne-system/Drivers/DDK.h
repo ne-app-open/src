77bdb1419e491a643a5ae8bcc853f77102bb0dee
Amlal <amlal@nekernel.org>
2026-05-01 17:32:23 +0200

    [CHORE] Update URL for DDK.h.

--- public/ne-system/Drivers/DDK.h
+++ public/ne-system/Drivers/DDK.h
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2026, Amlal El Mahrouss (amlal@nekernel.org)
 // Licensed under the Apache License, Version 2.0 (see LICENSE file)
-// Official repository: https://github.com/ne-foss/ne-system
+// Official repository: https://git.nekernel.org/ne-system
 
 #ifndef _NE_DDK_DDK_H_
 #define _NE_DDK_DDK_H_
