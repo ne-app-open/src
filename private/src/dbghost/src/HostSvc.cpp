a95012571168cd86a1f9030b74ac91ce809e63dd
Amlal El Mahrouss <amlal@nekernel.org>
2026-06-21 01:46:51 +0200

    [FEAT] src: update repository URL.

--- private/src/dbghost/src/HostSvc.cpp
+++ private/src/dbghost/src/HostSvc.cpp
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2026, Amlal El Mahrouss (amlal@nekernel.org)
 // Licensed under the Apache License, Version 2.0 (see LICENSE file)
-// Official repository: https://github.com/ne-foss/src
+// Official repository: https://github.com/ne-app-eu/src
 
 #include <DbgKit/Dbg.h>
 
