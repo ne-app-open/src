7e68c7b1b41dad248e15fed793204e8cdb9f1a12
Amlal El Mahrouss <amlal@nekernel.org>
2026-08-12 07:18:41 +0200

    [FEAT] src: Add resources file to system DLLs. (Part 3)
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

---
+++ private/CompilerKit/Version.h
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2026, Amlal El Mahrouss (amlal@nekernel.org)
+// Licensed under the Apache License, Version 2.0 (see LICENSE file)
+// Official repository: https://github.com/ne-app-eu/src
+
+#ifndef COMPILERKIT_VERSION_H
+#define COMPILERKIT_VERSION_H
+
+#define USR_VERSION "v1.1.4-nesystem"
+
+#define USR_EDITION "NeSystem OS Userspace (Standard Edition)"
+
+#define USR_VERSION_BCD (0x1104)
+#define USR_VERSION_RSRC 1,1,4,0
+
+#endif
