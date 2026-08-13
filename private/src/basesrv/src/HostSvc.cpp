b1371aac4df79e0e1327884b5e154aa5af01be4d
Amlal El Mahrouss <amlal@nekernel.org>
2026-08-13 19:03:56 +0200

    FEAT: src: Add basesrv module.

---
+++ private/src/basesrv/src/HostSvc.cpp
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2026, Amlal El Mahrouss (amlal@nekernel.org)
+// Licensed under the Apache License, Version 2.0 (see LICENSE file)
+// Official repository: https://github.com/ne-app-eu/src
+
+#include <SystemKit/System.h>
+#include <SystemKit/Err.h>
+
+SInt32 main(SInt32 argc, Char** argv) {
+  LIBSYS_UNUSED(argc);
+  LIBSYS_UNUSED(argv);
+
+  auto ret = kErrorSuccess;
+  return ret;
+}
