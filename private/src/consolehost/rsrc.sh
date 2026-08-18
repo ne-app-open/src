222462efa38489158a284f58a2702e800c39627e
Amlal El Mahrouss <amlal@nekernel.org>
2026-08-18 11:22:00 +0200

    FEAT: host programs such as the posix server, and dbghost compiles.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

---
+++ private/src/consolehost/rsrc.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+x86_64-w64-mingw32-windres -I../../ chs_rsrc.rsrc -O coff -o chs_rsrc.obj
---
+++ private/src/dbghost/rsrc.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+x86_64-w64-mingw32-windres -I../../ dbg_rsrc.rsrc -O coff -o dbg_rsrc.obj
