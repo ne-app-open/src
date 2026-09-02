d52a1030ec6c253e47e05be4146739b441fd3b08
Amlal El Mahrouss <amlal@nekernel.org>
2026-09-02 08:20:26 +0200

    CHORE: src: refactor CHS with upcoming UI logic
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

---
+++ private/src/consolehost/src/HostUI.cpp
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2026, Amlal El Mahrouss and Ne.app (amlal@nekernel.org)
+// Licensed under the Apache License, Version 2.0 (see LICENSE file)
+// Official repository: https://github.com/ne-app-eu/src
+
+#include <CHSKit/Console.h>
+#include <Ne/System/LWAS.h>
+
